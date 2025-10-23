869a409c809847b3e48bdf953a07b83336b8aa6c
Rafael <rafael.oliveira.silva09@escola.pr.gov.br>
2025-10-23 13:57:13 +0000

    aaaa

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import Titulo from './componentes/Titulo';
+import Saudacao from './componentes/Saudacao';
+
+export default function App(){
+  return(
+    <ScrollView style={{ padding:20}}>
+      <Titulo />
+      <Saudacao nome='Rafael"/>
+      <Saudacao nome="Gabriela"/>
+      <Saudacao nome="Rafaela"/>
+          
+
+    </ScrollView>
+  )
+}
---
+++ componentes/Saudacao.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Saudacao({nome}){
+    return(
+        <Text style={{fontSize:18, marginBottom:5}}>
+            Olá {nome}
+        </Text>
+    )
+}
