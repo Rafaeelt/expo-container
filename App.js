fb0df0209fc6a0945b3bfc1d6d61cf978d0ced66
Rafael <rafael.oliveira.silva09@escola.pr.gov.br>
2025-11-06 13:01:23 +0000

    11111111

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
+// App.js
 import React from 'react';
 import { ScrollView } from 'react-native';
-import Titulo from './componentes/Titulo';
-import Saudacao from './componentes/Saudacao';
+import Titulo from './Titulo';
+import Saudacao from './Saudacao';
+import FotoPerfil from './FotoPerfil';
+import CartaoUsuario from './CartaoUsuario';
 
-export default function App(){
-  return(
-    <ScrollView style={{ padding:20}}>
+export default function App() {
+  return (
+    <ScrollView style={{ padding: 20 }}>
       <Titulo />
-      <Saudacao nome="Rafael"/>
-      <Saudacao nome="Gabriela"/>
-      <Saudacao nome="Rafaela"/>
-          
-
+      <Saudacao nome="Rafael" />
+      <Saudacao nome="Aluno Tech" />
+      <FotoPerfil url="https://alunotech.com.br/" />
+      <CartaoUsuario
+        nome="Emanuelly"
+        descricao="Desenvolvedora em Formação"
+        imagem="https://joaoespacheco.vercel.app/"
+      />
     </ScrollView>
-  )
+  );
 }
